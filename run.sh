e712bc0f8719cf0ce3b7c61e428554353e3352b7
Thomas <tfrancois97@outlook.com>
2021-07-14 18:58:40 -0400

    bonne valeurs

--- run.sh
+++ run.sh
@@ -2,12 +2,12 @@ make all
 
 echo ""
 echo "<<<"
-mpirun -np 7 ./lab3 9 9 360 5 0.1 7
+mpirun -np 8 ./lab3 9 10 360 5 0.1 8
 echo ">>>"
 
 echo ""
 echo "<<<"
-mpirun -np 7 ./lab3 5 9 300 5 1 7
+mpirun -np 8 ./lab3 5 10 300 5 1 8
 echo ">>>"
 
 make clean
